bd5e5cb5ac40bf0e694cd0c90e533c6cd9fffb48
Bhupender Sharma <sharma05031989@gmail.com>
2025-01-03 23:22:03 +0530

    Details added

---
+++ comfy.py
@@ -0,0 +1,19 @@
+"""
+Power of GPU : Local hosted llm powering image generation on fly.
+
+Amazed to feel the power of GPU in my system. 
+Revolutionary AI image and video creation process is changing the game for content creators everywhere.
+ 🌟 With cutting-edge technology it is becoming easier than ever to bring your ideas to life. 
+ Say goodbye to complicated steps and multiple tools, and hello to stunning visuals, 
+ immersive animations, and engaging stories all in one streamlined workflow. 👩‍💻🎥
+
+Experimented with comfy along with custom hosted LLM, 
+It generated stunning images.
+ Custom node "if prompt" connects with local large language models to generate prompts for stable diffusion,
+  allowing for diverse image styles and creative storytelling. 🚀
+
+
+
+
+
+"""
